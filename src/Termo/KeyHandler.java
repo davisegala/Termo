e293676647a7395e68254be436d4cb1081c38d7e
davisegala123 <miojoaligenigena@gmail.com>
2026-06-22 15:05:04 -0300

    Add files via upload

---
+++ src/Termo/KeyHandler.java
@@ -0,0 +1,33 @@
+package Termo;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    private String tecla;
+    
+    @Override
+    public void keyTyped(KeyEvent ke) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        tecla = "";
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        int code = ke.getKeyCode();
+        String nomeTecla = KeyEvent.getKeyText(code);
+
+        if (code >= KeyEvent.VK_A && code <= KeyEvent.VK_Z) {
+            tecla = nomeTecla.toLowerCase();
+        }
+        
+        if (code == KeyEvent.VK_ENTER) {
+        }
+        
+        if (code == KeyEvent.VK_BACK_SPACE) {
+        }
+    }
+}
